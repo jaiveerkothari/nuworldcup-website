d14f8dbd716fac9fdbce3dfaf769f951b0a75925
AnxChow <ankitachowdhry97@gmail.com>
2017-01-31 20:27:34 -0600

    made it so that max 16 players can be added, alert if more than that

--- js/register.js
+++ js/register.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
   // Initialize the activityNumber
-  var playerNumber = 2;
+  var playerNumber = 3;
 
   // Select the add_activity button
   var addButton = document.getElementById("add_player");
@@ -11,11 +11,13 @@ window.onload = function() {
 
   // Attach handler to the button click event
   addButton.onclick = function() {
-
-  // Add a new row to the table using the correct activityNumber
-    playerTable.innerHTML +='<div class="container"><div class="row"><div class="col-sm-3">Player '+playerNumber+' Name* <br/> <input type="text" name="player'+playerNumber+'" > </div><div class="col-sm-3">Player '+playerNumber+' Email* <br><input type="text" name="player'+playerNumber+'" > </div><div class="col-sm-3">Has Player '+playerNumber+' ever been on the Northwestern Club Soccer Roster?<br><input type="radio" name="clubsoccer" value="clubsocceryes"> Yes<input type="radio" name="clubsoccer" value="clubsoccerno"> No </div></div></div>';
-
-    // Increment the activityNumber
-    playerNumber += 1;
+    if (playerNumber<16){
+      playerTable.innerHTML +='<div class="container"><div class="row"><div class="col-sm-3">Player '+playerNumber+' Name* <br/> <input type="text" name="player'+playerNumber+'" > </div><div class="col-sm-3">Player '+playerNumber+' Email* <br><input type="text" name="player'+playerNumber+'" > </div><div class="col-sm-3">Has Player '+playerNumber+' ever been on the Northwestern Club Soccer Roster?<br><input type="radio" name="clubsoccer" value="clubsocceryes"> Yes<input type="radio" name="clubsoccer" value="clubsoccerno"> No </div></div></div>';
+      playerNumber += 1;
+    }
+    else {
+      alert("Max 16 players");
+    }
   }
+
 };
